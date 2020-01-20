3897c5a69e1f3b3ef7b9a4d4678cbc74ddb69e2d
Volker Buzek <volker.buzek@js-soft.com>
2020-01-20 12:41:49 +0100

    perf: utilize stand-alone app

---
+++ docs-dev/webapp/Component.js
@@ -0,0 +1,25 @@
+sap.ui.define(
+    [
+        "sap/ui/core/UIComponent",
+        "sap/ui/core/ComponentSupport" // make sure to include the ComponentSupport in the bundle
+    ],
+    UIComponent => {
+        "use strict"
+
+        return UIComponent.extend("capcom.recap.Component", {
+            metadata: {
+                manifest: "json"
+            },
+
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
+            init() {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments)
+            }
+        })
+    }
+)
