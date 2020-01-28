05d3afcbea60f06c3dcdb872a9f195dfe35336b7
vobu <vobu@users.noreply.github.com>
2020-01-28 06:12:11 +0000

    docs: re>≡CAP website 53f8b2dddd016f052ba81bde87235b655b1f7653

--- docs/resources/sap/m/ComboBoxRenderer.js
+++ docs/resources/sap/m/ComboBoxRenderer.js
@@ -1,6 +1,6 @@
 /*!
  * OpenUI5
- * (c) Copyright 2009-2019 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
-sap.ui.define(["./ComboBoxBaseRenderer","sap/ui/core/Renderer"],function(s,a){"use strict";var e=a.extend(s);e.CSS_CLASS_COMBOBOX="sapMComboBox";e.addOuterClasses=function(a,d){s.addOuterClasses.apply(this,arguments);a.addClass(e.CSS_CLASS_COMBOBOX)};e.addInnerClasses=function(a,d){s.addInnerClasses.apply(this,arguments);a.addClass(e.CSS_CLASS_COMBOBOX+"Inner")};e.addButtonClasses=function(a,d){s.addButtonClasses.apply(this,arguments);a.addClass(e.CSS_CLASS_COMBOBOX+"Arrow")};e.addPlaceholderClasses=function(a,d){s.addPlaceholderClasses.apply(this,arguments);a.addClass(e.CSS_CLASS_COMBOBOX+"Placeholder")};return e},true);
+sap.ui.define(["./ComboBoxBaseRenderer","sap/ui/core/Renderer"],function(s,e){"use strict";var a=e.extend(s);a.apiVersion=2;a.CSS_CLASS_COMBOBOX="sapMComboBox";a.addOuterClasses=function(e,C){s.addOuterClasses.apply(this,arguments);e.class(a.CSS_CLASS_COMBOBOX)};a.addInnerClasses=function(e,C){s.addInnerClasses.apply(this,arguments);e.class(a.CSS_CLASS_COMBOBOX+"Inner")};a.addButtonClasses=function(e,C){s.addButtonClasses.apply(this,arguments);e.class(a.CSS_CLASS_COMBOBOX+"Arrow")};a.addPlaceholderClasses=function(e,C){s.addPlaceholderClasses.apply(this,arguments);e.class(a.CSS_CLASS_COMBOBOX+"Placeholder")};return a},true);
--- docs/resources/sap/ui/layout/VerticalLayoutRenderer.js
+++ docs/resources/sap/ui/layout/VerticalLayoutRenderer.js
@@ -1,6 +1,6 @@
 /*!
  * OpenUI5
- * (c) Copyright 2009-2019 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
-sap.ui.define([],function(){"use strict";var t={};t.render=function(t,e){var i=t;i.write("<div");i.writeControlData(e);i.addClass("sapUiVlt");i.addClass("sapuiVlt");if(e.getWidth()&&e.getWidth()!=""){i.addStyle("width",e.getWidth())}i.writeStyles();i.writeClasses();i.write(">");var r=e.getContent();for(var a=0;a<r.length;a++){i.write('<div class="sapUiVltCell sapuiVltCell">');i.renderControl(r[a]);i.write("</div>")}i.write("</div>")};return t},true);
+sap.ui.define([],function(){"use strict";var t={apiVersion:2};t.render=function(t,e){var i=t;i.openStart("div",e);i.class("sapUiVlt");i.class("sapuiVlt");if(e.getWidth()&&e.getWidth()!=""){i.style("width",e.getWidth())}i.openEnd();var n=e.getContent();for(var s=0;s<n.length;s++){i.openStart("div");i.class("sapUiVltCell");i.class("sapuiVltCell");i.openEnd();i.renderControl(n[s]);i.close("div")}i.close("div")};return t},true);
