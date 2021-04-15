54cb8f0f326880cb6aa02c93a28d80bd371c493b
Volker Buzek <volker.buzek@js-soft.com>
2021-04-15 14:47:50 +0200

    feat: basic skeleton

---
+++ docs/App.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller", "sap/ui/core/ComponentSupport"],
+    Controller => {
+        return Controller.extend("capcom.recap.App", {
+            onInit() { }
+
+        })
+    }
+)
