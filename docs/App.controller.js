30139652c07078eb3091c3f161d0954dad6d30d9
Volker Buzek <volker.buzek@js-soft.com>
2021-04-23 16:52:44 +0200

    feat: cfp

--- docs/App.controller.js
+++ docs/App.controller.js
@@ -8,38 +8,67 @@ sap.ui.define(
         return Controller.extend("capcom.recap.App", {
             onInit() {
                 const Model = new JSONModel({
-                    cli: `<p class="green terminal">$&gt; <input type="text" id="terminal" class="cmd terminal" placeholder="cds watch"></p>`
+                    cli: `<p class="green terminal">$&gt; <input type="text" id="terminal" class="cmd terminal" placeholder="cds deploy"></p>`
                 })
                 this.getView().setModel(Model)
             },
             onAfterRendering() {
                 const ui5 = this
                 const prompt = document.getElementById("terminal")
+                prompt.focus()
                 prompt.addEventListener("keydown", (event) => {
                     if (event.key === "Enter" || event.keyCode === 13) {
                         if (prompt.value === "cds watch") {
+                            ui5.byId("zsh").setVisible(false)
+                            ui5.typeIt("🐬 42 🪐")
+                        } else if (prompt.value === "cds deploy") {
                             ui5.byId("zsh").setVisible(false)
                             ui5.typeIt()
                         }
                     }
                 })
             },
-            typeIt() {
+            typeIt(m) {
                 const delay = (ms) => {
                     return new Promise((resolve) => setTimeout(resolve, ms))
                 }
-                const cursor = this.byId("output")
-                cursor.setVisible(true)
+                let cursor
                 const reducer = async (accumulator, curr) => {
                     const res = await accumulator
-                    await delay(200)
+                    await delay(150)
                     const acc = `${res}${curr}`
                     cursor.setText(acc)
                     return acc
                 }
-                ;`June 25th, 2021\nre>≡CAP online\n💚`
-                    .split("")
-                    .reduce(reducer, "")
+                if (m) {
+                    cursor = this.byId("output-past")
+                    cursor.setVisible(true)
+                    Promise.resolve()
+                        .then((_) => {
+                            return m.split("").reduce(reducer, "")
+                        })
+                        .then(async (_) => {
+                            await delay(1000)
+                            return cursor.setVisible(false)
+                        })
+                        .then((_) => {
+                            return this.byId("zsh").setVisible(true)
+                        })
+                        .finally((_) => {
+                            document.getElementById("terminal").value = ""
+                        })
+                } else {
+                    cursor = this.byId("output")
+                    cursor.setVisible(true)
+                    Promise.resolve()
+                        .then((_) => {
+                            return `➡️ CfP`.split("").reduce(reducer, "")
+                        })
+                        .finally(async (_) => {
+                            await delay(700)
+                            this.byId("output1").setVisible(true)
+                        })
+                }
             }
         })
     }
