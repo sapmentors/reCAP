7c3da476326ce004f89b87d420ed3861a6dbdf8d
Volker Buzek <volker.buzek@js-soft.com>
2021-06-10 16:22:43 +0200

    feat(agenda): skeleton

--- docs/App.controller.js
+++ docs/App.controller.js
@@ -6,46 +6,34 @@ sap.ui.define(
     ],
     (Controller, JSONModel) => {
         return Controller.extend("capcom.recap.App", {
-            onInit() {
+            daSpeakers(speakers) {
+                return speakers
+                    .map(
+                        (speaker) => `${speaker.firstName} ${speaker.lastName}`
+                    )
+                    .join(", ")
+            },
+            async onInit() {
                 const Model = new JSONModel({
                     cli: `<p class="green terminal">$&gt; <input type="text" id="terminal" class="cmd terminal" placeholder="cds info"></p>`
                 })
                 this.getView().setModel(Model)
-            },
-            onAfterRendering() {
-                const ui5 = this
-                const prompt = document.getElementById("terminal")
-                prompt.focus()
-                prompt.addEventListener("keydown", (event) => {
-                    if (event.key === "Enter" || event.keyCode === 13) {
-                        if (prompt.value === "cds info") {
-                            ui5.byId("zsh").setVisible(false)
-                            ui5.typeIt(`[CfP] done (> May 21)
-[sessions] confirmations > June 4
-[agenda] > June 11`)
-                        }
+
+                const _lineup = await fetch(
+                    `https://recap.cfapps.eu10.hana.ondemand.com/api/proposal/lineup`
+                ).then((r) => r.json())
+                // inject opening, keynote, lunch, afterevent
+                const lineup = _lineup.sort((a, b) => {
+                    if (a.startTime < b.startTime) {
+                        return -1
                     }
+                    if (a.startTime > b.startTime) {
+                        return 1
+                    }
+                    return 0
                 })
-            },
-            typeIt(m) {
-                const delay = (ms) => {
-                    return new Promise((resolve) => setTimeout(resolve, ms))
-                }
-                let cursor
-                const reducer = async (accumulator, curr) => {
-                    const res = await accumulator
-                    await delay(150)
-                    const acc = `${res}${curr}`
-                    cursor.setText(acc)
-                    return acc
-                }
-                if (m) {
-                    cursor = this.byId("output")
-                    cursor.setVisible(true)
-                    Promise.resolve().then((_) => {
-                        return m.split("").reduce(reducer, "")
-                    })
-                }
+                const LineupModel = new JSONModel(lineup)
+                this.getView().setModel(LineupModel, "LineupModel")
             }
         })
     }
