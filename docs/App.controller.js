49a8da860beadca5c1fbbb33be29f6de5fe88858
Volker Buzek <volker.buzek@js-soft.com>
2021-06-10 16:53:33 +0200

    feat: layout + cleanup

--- docs/App.controller.js
+++ docs/App.controller.js
@@ -19,10 +19,12 @@ sap.ui.define(
                 })
                 this.getView().setModel(Model)
 
-                const _lineup = await fetch(
+                let _lineup = await fetch(
                     `https://recap.cfapps.eu10.hana.ondemand.com/api/proposal/lineup`
                 ).then((r) => r.json())
                 // inject opening, keynote, lunch, afterevent
+                _lineup = [..._lineup, ...this.inject()]
+                console.log(_lineup)
                 const lineup = _lineup.sort((a, b) => {
                     if (a.startTime < b.startTime) {
                         return -1
@@ -34,6 +36,87 @@ sap.ui.define(
                 })
                 const LineupModel = new JSONModel(lineup)
                 this.getView().setModel(LineupModel, "LineupModel")
+            },
+
+            inject() {
+                const opening = {
+                    id: 47111,
+                    title: "Grande Opening",
+                    description: "re>≡CAP online 2021 starts!",
+                    location: "FRI",
+                    startTime: "09:00",
+                    endTime: "09:15",
+                    speakers: [
+                        {
+                            firstName: "DJ",
+                            lastName: "Adams",
+                            company: "SAP SE",
+                            bio: "",
+                            funFact: "",
+                            twitterHandle: "@qmacro",
+                            linkedInUrl: "",
+                            otherSocialMedia: null,
+                            photoUrl: ""
+                        },
+                        {
+                            firstName: "Volker",
+                            lastName: "Buzek",
+                            company: "j&uuml;s-soft GmbH",
+                            bio: "",
+                            funFact: "",
+                            twitterHandle: "@vobu",
+                            linkedInUrl: "",
+                            otherSocialMedia: null,
+                            photoUrl: ""
+                        }
+                    ],
+                    presentationLinks: []
+                }
+                const keynote = {
+                    id: 47112,
+                    title: "Keynote",
+                    description: "---",
+                    location: "FRI",
+                    startTime: "09:15",
+                    endTime: "10:00",
+                    speakers: [
+                        {
+                            firstName: "Daniel",
+                            lastName: "Hutzel",
+                            company: "SAP SE",
+                            bio: "",
+                            funFact: "",
+                            twitterHandle: "",
+                            linkedInUrl: "",
+                            otherSocialMedia: null,
+                            photoUrl: ""
+                        }
+                    ],
+                    presentationLinks: []
+                }
+                const beer = {
+                    id: 47113,
+                    title: "After-Event",
+                    description: "we all hang in a Zoom session and share a 🍹",
+                    location: "FRI",
+                    startTime: "16:00",
+                    endTime: "20:00",
+                    speakers: [
+                        {
+                            firstName: "#SAPCAP",
+                            lastName: "community",
+                            company: "the world",
+                            bio: "",
+                            funFact: "",
+                            twitterHandle: "",
+                            linkedInUrl: "",
+                            otherSocialMedia: null,
+                            photoUrl: ""
+                        }
+                    ],
+                    presentationLinks: []
+                }
+                return [opening, keynote, beer]
             }
         })
     }
