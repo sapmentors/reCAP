14682c0f59d36d3bf857a78e6e0d1f40aade59a6
Volker Buzek <volker.buzek@js-soft.com>
2022-03-11 17:31:43 +0100

    chore: move for dev

--- docs-dev/webapp/App.controller.js
+++ docs-dev/webapp/App.controller.js
@@ -1,19 +1,17 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller", "sap/ui/core/ComponentSupport"],
-    Controller => {
-        "use strict"
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel",
+        "sap/ui/core/ComponentSupport"
+    ],
+    (Controller, JSONModel) => {
         return Controller.extend("capcom.recap.App", {
-            onInit() {},
-            onAfterRendering() {
-                const sAgendaTableId = sap.ui.core.Fragment.createId(
-                    "AgendaFragment",
-                    "Agenda"
-                )
-                const oAgendaTable = this.byId(sAgendaTableId)
-                this.byId("toAgenda").setHref(`#${oAgendaTable.getId()}`)
-            },
-            onSponsorPress(sUrl) {
-                window.open(sUrl)
+            onInit() {
+                const oModel = new JSONModel({
+                    hereswhy: `<p>the pandemic isn't over - and as much as we'd like to have everyone come together in person,<br>
+              we can and will not risk to accidentally create a superspreader event</p>`
+                })
+                this.getView().setModel(oModel)
             }
         })
     }
