6d6e8ce22d2f49c11c130fd7fa5c133bcd856e4a
gregorwolf <gregorwolf@users.noreply.github.com>
2022-04-21 15:42:29 +0000

    docs: re>≡CAP website 661e3165ba25e867f2bcbecd28b06bb8d9a7a4b1

--- docs-dev/webapp/App.controller.js
+++ docs-dev/webapp/App.controller.js
@@ -1,20 +1,46 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller", "sap/ui/core/ComponentSupport"],
-    Controller => {
-        "use strict"
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel",
+        "sap/ui/Device",
+        "sap/ui/core/ComponentSupport",
+    ],
+    (Controller, JSONModel, Device) => {
         return Controller.extend("capcom.recap.App", {
-            onInit() {},
-            onAfterRendering() {
-                const sAgendaTableId = sap.ui.core.Fragment.createId(
-                    "AgendaFragment",
-                    "Agenda"
-                )
-                const oAgendaTable = this.byId(sAgendaTableId)
-                this.byId("toAgenda").setHref(`#${oAgendaTable.getId()}`)
+            onInit() {
+                // const deviceModel = new JSONModel(Device)
+                // deviceModel.setDefaultBindingMode("OneWay")
+                // this.setModel(deviceModel, "device")
+
+                const vpanel = this.byId("panelist")
+                if (Device.resize.width >= 697) {
+                    vpanel.setWidth("44%")
+                } else {
+                    vpanel.setWidth("88%")
+                }
+
+                Device.resize.attachHandler((params) => {
+                    console.log(`//> width: ${params.width}`)
+                    const vpanel = this.byId("panelist")
+                    if (params.width >= 697) {
+                        console.log("44%")
+                        vpanel.setWidth("44%")
+                    } else {
+                        console.log("88%")
+
+                        vpanel.setWidth("88%")
+                    }
+                })
+
+                const oModel = new JSONModel({
+                    hereswhy: `<p>the pandemic isn't over 🦠 - and as much as we'd like to have everyone come together in person, we can and will not risk to accidentally create a superspreader event 😪</p>
+                    <p>nevertheless, we'd like to offer <span class="glow">the selected speakers</span> 🗣 the opportunity <span class="glow">to come on-site</span> - to at least get that feeling of being on stage 🎤, presenting to an auditorium.<br>
+                    (plus you'll be offered to wear <span class="glow">re>≡CAP swag</span> 🤭😜)</p>
+                    <p>for such a manageable number of people, we can guarantee to have adequate safety and hygiene 🧴 measures in place - for the health 🤒 of everyone being at ROT.</p>
+                    <h3>🌍 all talks will both be streamed live and recorded!</h3>`,
+                })
+                this.getView().setModel(oModel)
             },
-            onSponsorPress(sUrl) {
-                window.open(sUrl)
-            }
         })
     }
 )
--- docs/resources/sap/ui/model/odata/v4/lib/_MinMaxHelper.js
+++ docs/resources/sap/ui/model/odata/v4/lib/_MinMaxHelper.js
@@ -1,6 +1,6 @@
 /*!
  * OpenUI5
- * (c) Copyright 2009-2021 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2022 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
-sap.ui.define(["./_AggregationHelper","./_Cache"],function(e,t){"use strict";return{createCache:function(n,r,s,u){var i={},a,o=false,c,h;a=t.create(n,r,u,true);c=new Promise(function(e){h=e});a.getMeasureRangePromise=function(){return c};a.getResourcePathWithQuery=function(t,n){var r=e.buildApply(s,Object.assign({},this.mQueryOptions,{$skip:t,$top:n-t}),1,o,i);o=true;return this.sResourcePath+this.oRequestor.buildQueryString(this.sMetaPath,r,false,true)};a.handleResponse=function(e,t,n,r){var s,u={},a=n.value.shift();function o(e){u[e]=u[e]||{};return u[e]}n["@odata.count"]=a.UI5__count;for(s in i){o(i[s].measure)[i[s].method]=a[s]}h(u);delete this.handleResponse;this.handleResponse(e,t,n,r)};return a}}},false);
+sap.ui.define(["./_Cache","./_ConcatHelper"],function(e,n){"use strict";return{createCache:function(r,t,u,a){var c={},i,o,f=new Promise(function(e){o=e});function s(e){var n,r={};function t(e){r[e]=r[e]||{};return r[e]}for(n in c){t(c[n].measure)[c[n].method]=e[n]}o(r)}i=e.create(r,t,a,true);n.enhanceCache(i,u,[s],c);i.getMeasureRangePromise=function(){return f};return i}}},false);
