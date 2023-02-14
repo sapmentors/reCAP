0a652de8bb29904fba39f905ccacb58b09d364bd
Volker Buzek <volker.buzek@js-soft.com>
2023-02-14 13:58:35 +0100

    feat: open cfp

--- docs-dev/webapp/App.controller.js
+++ docs-dev/webapp/App.controller.js
@@ -8,6 +8,7 @@ sap.ui.define(
         return Controller.extend("recap.App", {
             onInit() {
                 const oModel = new JSONModel({
+                    cfp: `&rarr; <a href="https://recap.cfapps.eu12.hana.ondemand.com">CfP (open</a> until Mar 31)`,
                     where: `<p>Our friends from <a href="https://openui5.org/ui5con/germany2023/">UI5con</a> rock their event the previous day, Jul 6.</p><p>So you &#x1FAF5; can get the full stack conference experience, back to back.</p>`,
                 })
                 this.getView().setModel(oModel)
